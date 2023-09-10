dd80470a0f37eecf1e9764af10ed56abc4017f1d
laquino <luiz.aquino@outlook.com>
2023-09-10 00:09:20 -0300

    Adicionar arquivos de migration

---
+++ StockWallet.Db.Mysql/Migrations/20230910005751_SummaryControl.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockWallet.Db.Mysql.Migrations
+{
+    /// <inheritdoc />
+    public partial class SummaryControl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LastProcessedId",
+                table: "Summaries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastProcessedId",
+                table: "Summaries");
+        }
+    }
+}
